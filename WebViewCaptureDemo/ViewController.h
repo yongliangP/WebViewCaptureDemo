f2827b8b6a7f5de3d469923cd99f6a23c46f05ad
yongliangPeng <1994577317@qq.com>
2016-09-06 16:05:35 +0800

    Initial Commit

---
+++ WebViewCaptureDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebViewCaptureDemo
+//
+//  Created by 彭永亮 on 16/9/6.
+//  Copyright © 2016年 yongliangP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WebViewCaptureDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WebViewCaptureDemo
+//
+//  Created by 彭永亮 on 16/9/6.
+//  Copyright © 2016年 yongliangP. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
